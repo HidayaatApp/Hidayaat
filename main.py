83bc81a62da298bfd22475cd290cc7d965e0873d
Shakil Hussain <shakil.hussain2022@gmail.com>
2025-04-18 21:27:13 -0600

    edit app routes

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# English pages
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/quiz')
 def quiz():
-    return render_template('quiz.html')  # this is your renamed quiz.html
-@app.route('/listen')
+    return render_template('quiz.html')
 
+@app.route('/listen')
 def listen():
     return render_template('listen.html')
 
-# You can later add:
-# @app.route('/recite') for audio features
+# Arabic pages
+@app.route('/ar/t')
+def ar_home():
+    return render_template('ar/index.html')
+
+@app.route('/ar/quiz')
+def ar_quiz():
+    return render_template('ar/quiz.html')
+
+@app.route('/ar/listen')
+def listen():
+    return render_template('ar/listen.html')
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
